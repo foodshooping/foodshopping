7aedb9c4066715be95a3c0d1e7bfcf7079342f04
foodshooping <foodshooping26@gmail.com>
2026-09-24 17:42:03 -0600

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = 'https://higpobsngppvehpldcoj.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_AqlLFw5QLLSfhnpqkjYgRA_9326cTHc';
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
